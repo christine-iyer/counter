7e7047ee10f9f89ad983a65bba141ca696d9f679
chrisIyer <hellohotbox420@gmail.com>
2023-10-15 12:37:15 -0400

    running into problems running app if I use ts files.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Counter from './components/Counter'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      Hi
+      </header>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/counterTwo.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import state from '../photos/state.png'
+
+export default function CounterTwo({count: number}) {
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      
+      <h1> What is a hook?</h1>
+      <p>A React hook is a special took that gains you access to special features in the package which make an app interactive with the user. .</p>
+      <h1>useState</h1>
+      <p> The useState hook lets you add state, i.e., bring life to, functional components.</p>
+      <h3>Component that 'uses state'</h3>
+      <p>The snippet below brings the button to life using state. Every time a user clicks the button, a display updates thee count of clicks. </p>
+      <div style={{ borderStyle: "solid", borderColor: 'yellow', width: '100px', padding: '10px', margin: '10px'}}>
+      <p>you clicked {count} times</p>
+      <button style={{color: 'ThreeDHighlight', backgroundColor: 'yellow'}}onClick={() => setCount(count + 1)}> Click</button>
+    </div>
+    <div>
+
+      <img src={state}  alt="" width='500'/>
+    </div>
+  </div>
+  )
+}
+
