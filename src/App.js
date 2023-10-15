41d6f72a7f8e7b59d1d0a1a217c315ff3ca257d6
chrisIyer <hellohotbox420@gmail.com>
2023-10-15 18:55:39 -0400

    got the odal going.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import { useState } from 'react';
 import Counter from './components/Counter'
-
+import ModalDialogue from './components/ModalDialogue'
 function App() {
+  const [show, setShow] = useState(false)
   return (
     <div className="App">
       <header className="App-header">
-      Hi
+        Hi
       </header>
       <Counter />
+      <>
+        <button onClick={() => setShow(true)}>Open
+        </button>
+        <ModalDialogue isOpen={show}>
+          Hi
+          <br />
+          <button onClick={() => 
+            setShow(false)}>Close</button>
+          
+        </ModalDialogue>
+      </>
+      
     </div>
+    
+
   );
 }
 
